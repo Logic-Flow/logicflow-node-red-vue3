04d0c6a575a0c7f9299911e497bb0852335f059d
xutao <towersxu@gmail.com>
2022-03-02 14:26:19 +0800

    feat: init logicflow

---
+++ src/components/FlowChart.vue
@@ -0,0 +1,36 @@
+<script>
+import { ref } from 'vue'
+import LogicFlow from '@logicflow/core'
+import '@logicflow/core/dist/style/index.css'
+
+export default {
+  props: {
+    msg: String
+  },
+  setup() {
+    const count = ref(0)
+    return {
+      count
+    }
+  },
+  mounted() {
+    console.log(44, this.count)
+    this.lf = new LogicFlow({
+      container: this.$refs.container,
+      grid: true
+    })
+    this.lf.render()
+  }
+}
+</script>
+
+<template>
+  <div ref="container" class="container"></div>
+</template>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,24 @@
-<script setup>
+<script>
 import { ref } from 'vue'
 
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
+export default {
+  props: {
+    msg: String
+  },
+  setup() {
+    const count = ref(0)
+    return {
+      count
+    }
+  },
+  mounted() {
+    console.log(44, this.con)
+  }
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <div ref="container"></div>
 </template>
 
 <style scoped>
